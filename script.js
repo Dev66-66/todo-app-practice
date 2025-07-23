a2588ef1409b5f5cec0b57c10d1e4e4cf5799fcb
Maksv <maksv@example.com>
2025-07-23 17:30:50 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('addTask').onclick = () => {
+    const input = document.getElementById('taskInput');
+    const taskText = input.value.trim();
+    if (taskText !== '') {
+        const li = document.createElement('li');
+        li.textContent = taskText;
+        document.getElementById('taskList').appendChild(li);
+        input.value = '';
+    }
+};
+
